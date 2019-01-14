3de8ecf602313c36b02d4de6e25f621cab300df0
42tinmar <42tinmar@gmail.com>
2019-01-14 18:27:45 +0100

    add commentaires

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -7,25 +7,22 @@ def shout(word)
 end
 
 def repeat(word, i=2)
-	return word + (" #{word}" * (i-1))
+	return word + (" #{word}" * (i-1)) #1er ocu sans espace & n-1 ocu avec espace
 end
 
 def start_of_word(word, i)
-	return word.byteslice(0, i)
+	return word.byteslice(0, i)#byteslice passe en arg les indices 
 end
 
 def first_word(phrase)
-	return phrase.slice(/\w+/)
+	return phrase.slice(/\w+/) #w+ = longueur d'un mot 
 end
 
-#def titleize(phrase)
-#	return phrase.capitalize.split(' ').replace { |e| e.capitalize if e.length > 3 }.join(' ')
-#end
 
 def titleize(phrase)
-	phrase_ = phrase.capitalize.split(' ')
-	phrase_.each_with_index do |e, i|
-		if e.length > 3
+	phrase_ = phrase.capitalize.split(' ') #debut de phrase -> maj
+	phrase_.each_with_index do |e, i| 
+		if e.length > 3 # moins de 3 lettres pas de maj
 			phrase_[i].replace(e.capitalize)
 		end
 	end
